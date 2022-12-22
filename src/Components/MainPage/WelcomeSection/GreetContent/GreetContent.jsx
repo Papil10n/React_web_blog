92c9b8f07a5dc887fbc97c66e342f0e41547ec54
Papil10n <nikEpt.co@gmail.com>
2022-12-22 22:01:57 +0200

    update 22 dec

--- src/Components/MainPage/WelcomeSection/GreetContent/GreetContent.jsx
+++ src/Components/MainPage/WelcomeSection/GreetContent/GreetContent.jsx
@@ -7,7 +7,7 @@ const GreetContent = () => {
             <div className={`${style.helloText} ${style.text}`}>
                 Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
                 consequat duis enim
-                velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                velit mollit. Exercitation veniam consequat sunt nostrud amet amet.
             </div>
             <button
                className={`${style.helloBtn} ${style.btn}`}>Download Resume
